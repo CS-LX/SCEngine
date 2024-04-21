22f919998819dcbdd6ffa0b914172628216e0fa1
LX.. <1659404657@qq.com>
2024-04-22 00:42:41 +0800

    实体浏览器可显示字段和属性

--- SCEngine/子窗口/WorldEnititesWindow.cs
+++ SCEngine/子窗口/WorldEnititesWindow.cs
@@ -12,6 +12,8 @@ using DarkUI.Docking;
 using Entity = GameEntitySystem.Entity;
 using Component = GameEntitySystem.Component;
 using Game;
+using System.Globalization;
+using System.Reflection;
 
 namespace SCEngine {
     public partial class WorldEnititesWindow : DarkToolWindow {
@@ -55,6 +57,9 @@ namespace SCEngine {
             ComponentCreature componentCreature = entity.FindComponent<ComponentCreature>(false);
             if (componentCreature != null) return componentCreature.DisplayName;
 
+            ComponentBlockEntity componentBlockEntity = entity.FindComponent<ComponentBlockEntity>(false);
+            if (componentBlockEntity != null) return "方块实体";
+
             return string.Empty;
         }
 
@@ -78,8 +83,129 @@ namespace SCEngine {
             propertriesGrid.SelectedObject = null;
             object? selectedObject = entitiesView.SelectedNodes.FirstOrDefault()?.Tag ?? null;
             if (selectedObject != null) {
-                propertriesGrid.SelectedObject = selectedObject;
+                // 使用自定义的 TypeConverter
+                var typeDescriptor = new AutoBrowsableTypeDescriptor(TypeDescriptor.GetProvider(selectedObject).GetTypeDescriptor(selectedObject), selectedObject.GetType());
+
+                propertriesGrid.SelectedObject = typeDescriptor;
             }
+        }
+    }
+
+    public class AutoBrowsableTypeDescriptor : CustomTypeDescriptor {
+        private readonly Dictionary<string, PropertyDescriptor> _propertyDescriptors;
+        private readonly Dictionary<string, PropertyDescriptor> _fieldDescriptors;
+
+        public AutoBrowsableTypeDescriptor(ICustomTypeDescriptor parent, Type type) : base(parent) {
+            _propertyDescriptors = new Dictionary<string, PropertyDescriptor>();
+            _fieldDescriptors = new Dictionary<string, PropertyDescriptor>();
+
+            // 获取类的所有属性
+            var properties = type.GetProperties();
+
+            // 创建属性的 PropertyDescriptor 集合
+            foreach (var prop in properties) {
+                _propertyDescriptors[prop.Name] = new PropertyPropertyDescriptor(prop);
+            }
+
+            // 获取类的所有字段
+            var fields = type.GetFields();
+
+            // 创建字段的 PropertyDescriptor 集合
+            foreach (var field in fields) {
+                _fieldDescriptors[field.Name] = new FieldPropertyDescriptor(field);
+            }
+        }
+
+        public override PropertyDescriptorCollection GetProperties() {
+            var properties = new List<PropertyDescriptor>();
+
+            // 添加属性到属性集合
+            foreach (var prop in _propertyDescriptors.Values) {
+                properties.Add(new CategoryPropertyDescriptor(prop, "Properties"));
+            }
+
+            // 添加字段到属性集合
+            foreach (var field in _fieldDescriptors.Values) {
+                properties.Add(new CategoryPropertyDescriptor(field, "Fields"));
+            }
+
+            return new PropertyDescriptorCollection(properties.ToArray());
+        }
+
+        public override PropertyDescriptorCollection GetProperties(Attribute[] attributes) {
+            return GetProperties();
+        }
+
+        private class PropertyPropertyDescriptor : PropertyDescriptor {
+            private readonly PropertyInfo _property;
+
+            public PropertyPropertyDescriptor(PropertyInfo property) : base(property.Name, null) {
+                _property = property;
+            }
+
+            public override Type PropertyType => _property.PropertyType;
+
+            public override bool CanResetValue(object component) => false;
+
+            public override object GetValue(object component) => _property.GetValue(component);
+
+            public override void ResetValue(object component) => throw new NotSupportedException();
+
+            public override void SetValue(object component, object value) => _property.SetValue(component, value);
+
+            public override bool ShouldSerializeValue(object component) => false;
+
+            public override bool IsReadOnly => false;
+
+            public override Type ComponentType => _property.DeclaringType;
+        }
+
+        private class FieldPropertyDescriptor : PropertyDescriptor {
+            private readonly FieldInfo _field;
+
+            public FieldPropertyDescriptor(FieldInfo field) : base(field.Name, null) {
+                _field = field;
+            }
+
+            public override Type PropertyType => _field.FieldType;
+
+            public override bool CanResetValue(object component) => false;
+
+            public override object GetValue(object component) => _field.GetValue(component);
+
+            public override void ResetValue(object component) => throw new NotSupportedException();
+
+            public override void SetValue(object component, object value) => _field.SetValue(component, value);
+
+            public override bool ShouldSerializeValue(object component) => false;
+
+            public override bool IsReadOnly => false;
+
+            public override Type ComponentType => _field.DeclaringType;
+        }
+
+        private class CategoryPropertyDescriptor : PropertyDescriptor {
+            private readonly PropertyDescriptor _originalDescriptor;
+            private readonly string _category;
+
+            public CategoryPropertyDescriptor(PropertyDescriptor originalDescriptor, string category) : base(originalDescriptor) {
+                _originalDescriptor = originalDescriptor;
+                _category = category;
+            }
+
+            public override string Category => _category;
+
+            // Forward all other PropertyDescriptor methods to the original descriptor
+            public override Type ComponentType => _originalDescriptor.ComponentType;
+            public override TypeConverter Converter => _originalDescriptor.Converter;
+            public override bool IsBrowsable => _originalDescriptor.IsBrowsable;
+            public override bool IsReadOnly => _originalDescriptor.IsReadOnly;
+            public override Type PropertyType => _originalDescriptor.PropertyType;
+            public override bool CanResetValue(object component) => _originalDescriptor.CanResetValue(component);
+            public override object GetValue(object component) => _originalDescriptor.GetValue(component);
+            public override void ResetValue(object component) => _originalDescriptor.ResetValue(component);
+            public override void SetValue(object component, object value) => _originalDescriptor.SetValue(component, value);
+            public override bool ShouldSerializeValue(object component) => _originalDescriptor.ShouldSerializeValue(component);
         }
     }
 }
