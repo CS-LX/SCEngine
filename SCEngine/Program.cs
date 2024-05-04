915268bbcbfdb6331d6ce84f5e22c10dcb61827b
LX.. <1659404657@qq.com>
2024-05-04 12:59:46 +0800

    小优化

--- SCEngine/Program.cs
+++ SCEngine/Program.cs
@@ -2,6 +2,8 @@ namespace SCEngine;
 
 public static class Program {
     public static Thread SCThread;
+    public static Thread UpdateThread;
+    public static bool GameStarted = false;
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
@@ -10,8 +12,22 @@ public static class Program {
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
+        //打开SC
         SCThread = new Thread(() => Game.Program.EntryPoint());
         SCThread.Start();
+        GameStarted = true;
+        //打开更新
+        UpdateThread = new Thread(Update);
+        UpdateThread.Start();
+
         Application.Run(new MainForm());
+    }
+
+    static void Update() {
+        while (GameStarted == true && SCThread.IsAlive) {
+
+        }
+        //游戏关闭了就退出
+        Application.Exit();
     }
 }
